7184c41b457ee200a16361d10f162165a7b4732a
malleshreddy3313 <malleshreddy3313@gmail.com>
2025-07-20 12:19:54 +0530

    check Array is Sorted or not

--- src/Arrays/IsSorted.java
+++ src/Arrays/IsSorted.java
@@ -6,7 +6,7 @@ public class IsSorted {
     static boolean isSorted(int arr[], int n) {
         for (int i = 1; i < n; i++) {
             if (arr[i] < arr[i - 1])
-                return false;
+                return  false;
         }
 
         return true;
