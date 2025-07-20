e9fcc94752ab7040d02fcffbe02e12313762de1f
malleshreddy3313 <malleshreddy3313@gmail.com>
2025-07-20 14:28:21 +0530

    Remove Duplicates form the Array

---
+++ src/Arrays/LeftRotate.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.util.Scanner;
+
+//left rotate the given array by 1 digit
+//Time - complexity - O(N0
+public class LeftRotate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        left_rotate(arr, n);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void left_rotate(int[] arr, int n) {
+        int temp = arr[0];
+        for(int i=1;i < n;i++){
+            arr[i-1]=arr[i];
+        }
+        arr[n-1]=temp;
+    }
+}
