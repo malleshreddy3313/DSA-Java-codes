2828a39d24e028322cf0f7dbd10436478ddb8782
malleshreddy3313 <malleshreddy3313@gmail.com>
2025-08-09 16:59:26 +0530

    Minimun element in the rotated array

--- src/BinarySearch/MinInRotatedArray.java
+++ src/BinarySearch/MinInRotatedArray.java
@@ -3,14 +3,11 @@ package BinarySearch;
 class MinInRotatedArray{
     public int findMin(int[] nums) {
         int left = 0, right = nums.length - 1;
-
         while (left < right) {
             if (nums[left] < nums[right]) {
                 return nums[left];
             }
-
             int mid = left + (right - left) / 2;
-
             if (nums[mid] >= nums[left]) {
                 left = mid + 1;
             } else {
