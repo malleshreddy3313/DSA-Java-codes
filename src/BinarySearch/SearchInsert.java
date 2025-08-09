be360a2ed560f4286a8e903aec0dd9f8704650f8
malleshreddy3313 <malleshreddy3313@gmail.com>
2025-08-09 13:04:42 +0530

    Search the Index where the element need to be inserted.

---
+++ src/BinarySearch/SearchInsert.java
@@ -0,0 +1,24 @@
+package BinarySearch;
+
+
+//Leetcode:https://leetcode.com/problems/search-insert-position/
+class SearchInsert {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0 ;
+        int right = n-1;
+
+        int ans = n;
+        while(left<=right){
+            int mid = (left+right)/2;
+
+            if(nums[mid] >= target){
+                ans = mid;
+                right = mid - 1;
+            }else{
+                left = mid+1;
+            }
+        }
+        return ans;
+    }
+}
